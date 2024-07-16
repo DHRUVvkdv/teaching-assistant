ff5dfd71ac556d4968c9b02968c3d21025dd4624
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2024-07-16 18:12:45 -0400

    fix: updated the timeout for lambda and removed unnecessary files

--- ta-cdk-infra/lib/ta-cdk-infra-stack.ts
+++ ta-cdk-infra/lib/ta-cdk-infra-stack.ts
@@ -54,7 +54,7 @@ export class TaCdkInfraStack extends cdk.Stack {
     const apiFunction = new DockerImageFunction(this, "ApiFunc", {
       code: apiImageCode,
       memorySize: 256,
-      timeout: cdk.Duration.seconds(30),
+      timeout: cdk.Duration.seconds(60),
       architecture: Architecture.ARM_64,
       environment: {
         TABLE_NAME: teachingAssistantTavilyQueryTable.tableName,
